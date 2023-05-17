0e181686c4b6eff54b89648f648ed6cf32be0925
abhilekhgautam <abhilekhgautam119@gmail.com>
2023-05-17 16:30:11 +0545

    delete nodes with no child or only one child

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ struct Node{
 };
 class BST{
     Node* root;
+    Node* get_parent(Node*);
 
 public:
     BST(){
@@ -24,9 +25,30 @@ public:
     }
     void insert_data(float val);
     void inorder_traverse(Node* root);
-    void search_data(float val);
+    Node* search_data(float val);
     void delete_data(float val);
 };
+
+// risky: the provided node must exist
+// returns the parent of the given node (not the root of the tree)
+Node* BST::get_parent(Node* node) {
+    if (node == root){
+        return nullptr;
+    }
+    auto temp = root;
+    Node* parent = nullptr;
+    while (temp != nullptr){
+        parent = temp;
+        if (temp->right->val == node->val || temp->left->val == node->val){
+            return parent;
+        }
+        else if(node->val < temp->val){
+            temp = temp->left;
+        } else if (node->val > temp->val){
+            temp = temp->right;
+        }
+    }
+}
 
 void BST::insert_data(float val) {
 
@@ -70,12 +92,12 @@ void BST::inorder_traverse(Node* root) {
     }
 }
 
-void BST::search_data(float val) {
+Node* BST::search_data(float val) {
     auto temp = root;
     while (temp != nullptr){
         if (temp->val == val){
             std::cout << "The value" << val << " is found\n";
-            return; // value found so return early
+            return temp; // value found so return early
         }
         else if(val < temp->val){
             temp = temp->left;
@@ -85,7 +107,54 @@ void BST::search_data(float val) {
     }
     // if you complete the loop no such value is present.
     std::cout << val << " not found\n";
+    return temp;
 }
+
+void BST::delete_data(float val) {
+    // check if the value exist
+    auto delete_node = search_data(val);
+    if(delete_node != nullptr){
+        auto parent = get_parent(delete_node);
+        std::cout << "Parent value: " << parent->val << "\n";
+        // if the value exists
+        // There are 3 cases
+        //1. if the node with the value has no children, just remove the node
+        if(delete_node->right == nullptr &&  delete_node->left == nullptr){
+            if(delete_node->val > parent->val)
+              parent->right = nullptr;
+            else
+                parent->left = nullptr;
+
+            delete delete_node; // risky: clean up thing
+        }
+        // 2. if the node has only one child, set the child as parent
+        else if(delete_node->right == nullptr || delete_node->left == nullptr) {
+            if (delete_node->right != nullptr){
+                // set delete_node -> right as a parent
+                if(delete_node->right->val > parent->val)
+                  parent->right = delete_node->right;
+                else
+                    parent->left = delete_node->right;
+
+            } else if (delete_node-> left != nullptr){
+                // set delete_node -> left as a parent.
+                if(delete_node->left->val > parent->val)
+                   parent->right = delete_node->left;
+                else
+                    parent->left = delete_node->left;
+            }
+
+            delete delete_node;
+        }
+        // 3. if the node has both children,
+        else{
+
+        }
+    } else {
+        std::cout << "Couldn't delete, " << val << " :no such value exists";
+    }
+}
+
 
 int main() {
 
@@ -97,10 +166,12 @@ int main() {
     my_btree->insert_data(4.0f);
     my_btree->insert_data(2.0f);
 
+    std::cout << "Before Deletion\n";
     my_btree->inorder_traverse(my_btree->get_root());
 
-    my_btree->search_data(55.0f);
+    my_btree->delete_data(4.0f);
+    std::cout << "After Deletion\n";
+    my_btree->inorder_traverse(my_btree->get_root());
 
-    my_btree->search_data(3.0f);
     return 0; // happy compiler :)
 }
